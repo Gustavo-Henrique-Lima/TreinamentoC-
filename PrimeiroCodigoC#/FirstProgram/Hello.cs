4a1d1fdf4140ee4fe2d99697276f63369ee94a20
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-04-16 17:35:15 -0300

    Escrevendo primeiro código C#

---
+++ PrimeiroCodigoC#/FirstProgram/Hello.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Hello
+{
+    static void Main()
+    {
+        Console.WriteLine("Olá mundo!");
+        Console.WriteLine("This is the first line");
+        Console.Write("This is");
+        Console.Write("the second line");
+    }
+}
