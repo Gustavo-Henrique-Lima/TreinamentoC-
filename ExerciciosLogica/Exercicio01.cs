a41329bf8f72506c3c52aaa0ea0fbad4bfff2aed
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-04-23 18:14:38 -0300

    Exericios de logica para treinar os conceitos do c#

---
+++ ExerciciosLogica/Exercicio01.cs
@@ -0,0 +1,20 @@
+/*
+namespace ExerciciosLogica
+{
+    class Exercicio01
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Digite um número para saber se ele é ímpar ou par: ");
+            int numero=Convert.ToInt32(Console.ReadLine());
+            if(numero%2==1)
+            {
+                Console.WriteLine("O número digitado foi "+numero+" e ele é ímpar");
+            }
+            else
+            {
+                Console.WriteLine("O número digitado foi "+numero+" e ele é par");
+            }
+        }
+    }
+}*/
---
+++ ExerciciosLogica/Exercicio02.cs
@@ -0,0 +1,12 @@
+namespace ExerciciosLogica
+{
+    class Exercicio02
+    {
+        static void Main(string[]args)
+        {
+          Console.WriteLine("Para continuarmos digite seu nome: ");
+          string nome=Console.ReadLine();
+          Console.WriteLine("Seja bem-vindo "+nome);
+        }
+    }
+}
